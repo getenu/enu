cc3663cee25dcc6afd58c63eaedcdee6a4123623
Scott Wadden <scott.wadden@gmail.com>
2024-10-25 22:31:53 -0300

    Jump buttons for touch controller

--- src/ui/gui.nim
+++ src/ui/gui.nim
@@ -1,17 +1,46 @@
 import pkg/godot
 import
-  godotapi/
-    [control, input_event_screen_touch, input_event_screen_drag, scene_tree]
+  godotapi/[
+    control, input_event_screen_touch, input_event_screen_drag, scene_tree,
+    input_event_action, input
+  ]
 import core, nodes/player_node, gdutils
 
 gdobj GUI of Control:
-  var left_stick: Control
+  var
+    left_stick: Control
+    up: Control
+    down: Control
+
   method ready() =
+    self.left_stick = find("LeftStick", Control)
+    self.up = find("Up", Control)
+    self.down = find("Down", Control)
+
     self.bind_signals self,
       "mouse_entered", "mouse_exited", "focus_entered", "focus_exited"
-    self.left_stick = find("LeftStick", Control)
+
+    for button in [self.up, self.down]:
+      self.bind_signal(button, "button_up", button.name)
+      self.bind_signal(button, "button_down", button.name)
+
     state.local_flags.changes:
       self.left_stick.visible = TouchControls in state.local_flags
+      self.up.visible = TouchControls in state.local_flags
+      self.down.visible =
+        TouchControls in state.local_flags and Flying in state.local_flags
+
+  method on_button_up(name: string) =
+    var ev = gdnew[InputEventAction]()
+    ev.action = if name == "Up": "jump" else: "crouch"
+    ev.pressed = false
+    parse_input_event(ev)
+
+  method on_button_down(name: string) =
+    var ev = gdnew[InputEventAction]()
+    ev.action = if name == "Up": "jump" else: "crouch"
+    ev.pressed = true
+    parse_input_event(ev)
 
   method on_mouse_entered() =
     state.push_flag ViewportFocused
