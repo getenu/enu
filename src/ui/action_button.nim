e0f0dc52faacf643e2b3567c2ec7feb2163c3cf0
Scott Wadden <scott.wadden@gmail.com>
2024-10-28 21:17:49 -0300

    Constrain toolbar to viewport width

--- src/ui/action_button.nim
+++ src/ui/action_button.nim
@@ -1,14 +1,16 @@
 import
   godotapi/[
     button, style_box_flat, input_event_screen_touch, input_event_screen_drag,
-    scene_tree
+    scene_tree, viewport,
   ]
 import godot
 import ".."/[core, gdutils]
 
 gdobj ActionButton of Button:
   proc update_size(size: float) =
-    let toolbar_size = state.config.toolbar_size * state.config.screen_scale
+    var toolbar_size = state.config.toolbar_size * state.config.screen_scale
+    if (toolbar_size + 4) * 8 > self.get_viewport().size.x:
+      toolbar_size = self.get_viewport().size.x / 8 - 4
     self.rect_min_size = vec2(toolbar_size, toolbar_size)
     for style in ["hover", "pressed", "focus", "normal"]:
       var stylebox = self.get_stylebox(style).as(StyleBoxFlat)
@@ -21,6 +23,10 @@ gdobj ActionButton of Button:
 
     self.update_size state.config.toolbar_size
     self.bind_signals self, "pressed"
+    self.bind_signals self.get_viewport(), "size_changed"
+
+  method on_size_changed() =
+    self.update_size(state.config.toolbar_size)
 
   method on_pressed*() =
     self.get_parent.trigger("action_changed", self.name)
