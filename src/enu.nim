d0801090052de80131b2a2d2318011558c43f191
dsrw <scott.wadden@gmail.com>
2025-05-05 23:42:08 -0300

    Removed `segfaults`

--- src/enu.nim
+++ src/enu.nim
@@ -1,17 +1,16 @@
 {.warning[UnusedImport]: off.}
 
-import libs/segfaults
 import core, game
 
 import
   ui/[
     editor, console, toolbar, action_button, preview_maker, markdown_label,
-    right_panel, settings, virtual_joystick, gui, floating_button
+    right_panel, settings, virtual_joystick, gui, floating_button,
   ]
 import
   nodes/[
     player_node, aim_target, selection_area, bot_node, ground_node, build_node,
-    sign_node
+    sign_node,
   ]
 
 Zen.bootstrap
