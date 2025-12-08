599dde0dd62315ae6a3dc2444566522fb03f9d46
Scott Wadden <scott.wadden@gmail.com>
2025-12-08 11:09:13 -0400

    Send keepalives. Use 2s script timeout after the initial load is done.

--- src/controllers/script_controllers/vars.nim
+++ src/controllers/script_controllers/vars.nim
@@ -2,5 +2,6 @@ import core
 
 const
   error_code* = some(99)
-  script_timeout* = 10.0.seconds
+  initial_script_timeout* = 10.0.seconds
+  script_timeout* = 2.0.seconds
   advance_step* = 0.5.seconds
